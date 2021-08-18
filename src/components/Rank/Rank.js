98a57493a95afae0232141c895f93b948d7e134b
Juan <pront23@gmail.com>
2021-08-18 13:46:01 +0900

    formatted file

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,14 +3,12 @@ import React from 'react';
 const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+      <div className="white f3">
+        {`${name}, your current count of image urls submitted is...`}
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
+      <div className="white f1">{entries}</div>
     </div>
   );
-}
+};
 
 export default Rank;
